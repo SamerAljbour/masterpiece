2831b3d2a1da4b1f4749efd9bc79e511cc66dc45
SamerAljbour <samerseran34@gmail.com>
2024-08-29 16:05:18 +0300

    added the admin template with the route view

--- e-commerce/routes/web.php
+++ e-commerce/routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// <================================= view for the pages ============================================>
+// <================================= view for the pages home ============================================>
 Route::get('/home', function () {
     return view('index');
 });
@@ -44,4 +44,67 @@ Route::get('/home-02', function () {
 Route::get('/home-03', function () {
     return view('home-03');
 });
-// <================================= End of view for the pages ============================================>
+// <================================= End of view for the pages home ============================================>
+
+// <================================= view for the pages admin ============================================>
+Route::get('/adminDashboard', function () {
+    return view('adminDashboard/index');
+});
+Route::get('/adminDashboard/sidebarstyle2', function () {
+    return view('adminDashboard/sidebar-style-2');
+});
+Route::get('/adminDashboard/starter-template', function () {
+    return view('adminDashboard/starter-template');
+});
+Route::get('/adminDashboard/icon-menu', function () {
+    return view('adminDashboard/icon-menu');
+});
+Route::get('/adminDashboard/widgets', function () {
+    return view('adminDashboard/widgets');
+});
+Route::get('/adminDashboard/tables/tables', function () {
+    return view('adminDashboard/tables/tables');
+});
+Route::get('/adminDashboard/tables/datatables', function () {
+    return view('adminDashboard/tables/datatables');
+});
+Route::get('/adminDashboard/maps/googlemaps', function () {
+    return view('adminDashboard/maps/googlemaps');
+});
+Route::get('/adminDashboard/maps/jsvectormap', function () {
+    return view('adminDashboard/maps/jsvectormap');
+});
+Route::get('/adminDashboard/charts/sparkline', function () {
+    return view('adminDashboard/charts/sparkline');
+});
+Route::get('/adminDashboard/charts/charts', function () {
+    return view('adminDashboard/charts/charts');
+});
+Route::get('/adminDashboard/components/avatars', function () {
+    return view('adminDashboard/components/avatars');
+});
+Route::get('/adminDashboard/components/buttons', function () {
+    return view('adminDashboard/components/buttons');
+});
+Route::get('/adminDashboard/components/font-awesome-icons', function () {
+    return view('adminDashboard/components/font-awesome-icons');
+});
+Route::get('/adminDashboard/components/gridsystem', function () {
+    return view('adminDashboard/components/gridsystem');
+});
+Route::get('/adminDashboard/components/notifications', function () {
+    return view('adminDashboard/components/notifications');
+});
+Route::get('/adminDashboard/components/panels', function () {
+    return view('adminDashboard/components/panels');
+});
+Route::get('/adminDashboard/components/simple-line-icons', function () {
+    return view('adminDashboard/components/simple-line-icons');
+});
+Route::get('/adminDashboard/components/sweetalert', function () {
+    return view('adminDashboard/components/sweetalert');
+});
+Route::get('/adminDashboard/components/typography', function () {
+    return view('adminDashboard/components/typography');
+});
+// <=================================  End view for the pages admin ============================================>
